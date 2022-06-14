69c2e92989dc0e540918ab746b6e5e8f3fb07fde
unknown <lbg>
2022-06-14 16:14:30 +0900

    fisrt commit

---
+++ src/views/DataBindingList2.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+ <table>
+   <thead>
+     <tr>
+       <th>제품명</th>
+       <th>가격</th>
+       <th>카테고리</th>
+       <th>배송료</th>
+     </tr>
+   </thead>
+   <tbody>
+     <tr :key="i" v-for="(product,i) in productList">
+       <td>{{product.product_name}}</td>
+       <td>{{product.price}}</td>
+       <td>{{product.category}}</td>
+       <td>{{product.delivery_price}}</td>
+     </tr>
+   </tbody>
+ </table>
+</div>
+</template>
+<script>
+export default {
+data() {
+  return {
+     productList: []
+  };
+},
+created() {
+  this.getList();
+},
+methods: {
+  async getList() {
+    alert(await this.$api("https://www.google.co.kr","get"));
+    this.productList = await this.$api("https://ddce4598-7fd1-4fc0-8b75-da2bb8ad5aa9.mock.pstmn.io/list","get");
+  }
+}
+}
+</script>
+<style scoped>
+table {
+ font-family: arial, sans-serif;
+ border-collapse: collapse;
+ width: 100%;
+}
+ 
+td, th {
+ border: 1px solid #dddddd;
+ text-align: left;
+ padding: 8px;
+}
+</style>
